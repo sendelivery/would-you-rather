7441c7cd5c8e1e05bbdded23df8cf5381990bb0f
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-11-01 21:13:20 +0000

    Event handling file structure and refactor

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,27 +1,21 @@
-const { readdirSync} = require('fs');
-const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 
 require("dotenv").config();
 
-const commands = [];
-const commandFiles = readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+const registerCommands = (client) => {
+  const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
-}
+  rest
+    .put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: client.commands.map((command) => command.data.toJSON()) }
+    )
+    .then(() => console.log("Successfully registered application commands."))
+    .catch(console.error);
+};
 
-const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
-
-rest
-  .put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID,
-      process.env.GUILD_ID
-    ),
-    { body: commands }
-  )
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+exports.registerCommands = registerCommands;
---
+++ src/events/ready.js
@@ -0,0 +1,10 @@
+const { registerCommands } = require('../deploy-commands');
+
+module.exports = {
+  name: "ready",
+  once: "true",
+  execute(client) {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+    registerCommands(client);
+  },
+};
